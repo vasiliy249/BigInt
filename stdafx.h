d43850024cf7e0ac4562a74e177efcad566bb76a
vasiliy249 <v.ivanischev@gmail.com>
2017-11-29 23:28:07 +0300

    add cmake gitignore and precompiled headers

---
+++ stdafx.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define NOMINMAX
+
+#include <iostream>
+#include <Windows.h>
+#include <thread>
+#include <string>
+#include <limits>
+#include <algorithm>
+#include <random>
+#include "boost/iostreams/device/mapped_file.hpp"
+#include "boost/program_options.hpp"
+#include "boost/filesystem.hpp"
